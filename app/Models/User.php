3a46f76978cc87247729cb348948f19437f544ba
Alessandro Olivares <alessandro.olivares@utp.ac.pa>
2025-05-01 21:17:05 -0500

    feat: update application structure and improve configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,36 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var list<string>
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -25,7 +41,8 @@ class User extends Model
     protected function casts(): array
     {
         return [
-            'id' => 'integer',
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
         ];
     }
 
